de6a8914a3ae991560dcd941f747972fe53cfb2a
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 21:03:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,11 @@ class Student
   
   
   def self.save 
+    sql = <<-SQL 
+      INSERT 
+    
   end 
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
